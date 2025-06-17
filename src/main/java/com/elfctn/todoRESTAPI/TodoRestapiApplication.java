7d1c52fc19f324ebbce7898cbb5b838797fd86a1
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-17 23:12:49 +0300

    feat: Initial Spring Boot project setup for To-Do List API

---
+++ src/main/java/com/elfctn/todoRESTAPI/TodoRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.elfctn.todoRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elfctn/todoRESTAPI/TodoRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elfctn.todoRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
